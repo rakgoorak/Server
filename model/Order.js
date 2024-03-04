771c83c5a63511bae5ca89b93d673df8e621bf23
rakgoorak <30443791+rakgoorak@users.noreply.github.com>
2024-03-05 03:45:45 +0700

    Add files via upload

---
+++ model/Order.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const OrderSchema = new mongoose.Schema(
+    {
+        products: [
+            {
+                product: {
+                    type: ObjectId,
+                    ref: "product",
+                },
+                name: String,
+                count: Number,
+                price: Number,
+            },
+        ],
+        cartTotal: Number,
+        fulladdress: {
+            houseNumber: String,
+            subdistrict: String,
+            district: String,
+            province: String,
+            zipcode: String,
+        },
+        phoneNumber: String,
+        name: String,
+        orderstatus: {
+            type: String,
+            default: "Processing",
+        },
+        orderBy: {
+            type: ObjectId,
+            ref: "users",
+        },
+        images: {
+            type: Array,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = Order = mongoose.model("order", OrderSchema);
